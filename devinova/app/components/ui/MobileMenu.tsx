e42354a775e1ee09bf24d416abe596850780eebd
SaraPellnor <sara.ahlquist@hotmail.com>
2024-05-08 22:18:43 +0200

    mobile menu list

--- devinova/app/components/ui/MobileMenu.tsx
+++ devinova/app/components/ui/MobileMenu.tsx
@@ -2,9 +2,11 @@
 import React, { useState, useEffect } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdArrowRoundForward } from "react-icons/io";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
-const MobileMenu = () => {
+const MobileMenu = ({ data }: { data: any }) => {
   const [open, setOpen] = useState(false);
+  const [subOpen, setSubOpen] = useState(false);
 
   return (
     <div className="relative text-neutralGrey lg:hidden overflow-hidden">
@@ -16,7 +18,7 @@ const MobileMenu = () => {
         ></div>
       )}
       <div
-        className={`fixed top-0 bg-gray-700 z-50 w-[40vw] h-screen transition-all duration-500 ${
+        className={`fixed p-10 pt-32 top-0 bg-gray-700 z-50 w-[40vw] h-screen transition-all duration-500 ${
           open ? "right-0" : "-right-[40vw]"
         } `}
       >
@@ -24,6 +26,46 @@ const MobileMenu = () => {
           onClick={() => setOpen(!open)}
           className="absolute top-8 right-8 text-2xl"
         />
+        <div>
+          <ul className="flex flex-col gap-3 text-xl">
+            {data.map((item: any, index: number) =>
+              item.subMenu ? (
+                <div key={index}>
+                  <div className="flex items-center">
+                    <p
+                      className="hover:border-b-4 border-violet-400 transition-all duration-100 ease-in-out"
+                      onClick={() => setSubOpen(!subOpen)}
+                    >
+                      {item.label}
+                    </p>
+
+                    <RiArrowDropDownLine className="text-5xl" />
+                  </div>
+                  <div
+                    className={` grid ${
+                      subOpen ? "grid-rows-[1fr]" : " grid-rows-[0fr]"
+                    } duration-500`}
+                  >
+                    <div
+                      className={`overflow-hidden dark:text-neutralGrey duration-1000`}
+                    >
+                      {item.subMenu.map((item: any, index: number) => (
+                        <p key={index}>{item.label}</p>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <li
+                  className=" hover:border-b-4 border-violet-400 transition-all duration-100 ease-in-out"
+                  key={index}
+                >
+                  {item.label}
+                </li>
+              )
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   );
--- devinova/app/components/ui/NavBar.tsx
+++ devinova/app/components/ui/NavBar.tsx
@@ -4,36 +4,14 @@ import { MdNightlight } from "react-icons/md";
 import { MdOutlineLightMode } from "react-icons/md";
 import { RiArrowDropDownLine } from "react-icons/ri";
 
-const NavBar = () => {
+const NavBar = ({ data }: { data: any }) => {
   const [open, setOpen] = useState("");
   const [hoverd, setHoverd] = useState("");
 
-  const menu = [
-    {
-      label: "Home",
-      link: "#",
-    },
-    { label: "New Nova", link: "#" },
-    { label: "Career", link: "#" },
-    {
-      label: "Services",
-      link: "#",
-      subMenu: [
-        {
-          label: "Services1",
-          link: "#",
-        },
-        { label: "Services2", link: "#" },
-        { label: "Services3", link: "#" },
-      ],
-    },
-    { label: "About", link: "#" },
-    { label: "Contact", link: "#" },
-  ];
   return (
     <div className="lg:block hidden">
       <ul className="flex gap-5 items-center text-neutralGrey">
-        {menu.map((item, index) => (
+        {data.map((item: any, index: number) => (
           <li
             className={`${
               !item.subMenu &&
@@ -68,9 +46,9 @@ const NavBar = () => {
                     open ? "flex" : "hidden"
                   }  absolute top-10 -left-3 flex-col gap-2 bg-gray-700 p-3 bg-opacity-50`}
                 >
-                  {item.subMenu.map((item, index) => (
+                  {item.subMenu.map((item: any, index: number) => (
                     <a
-                      className="hover:border-b-4 transition-all duration-100 ease-in-out border-violet-400"
+                      className="hover:border-b-4 border-violet-400 transition-all duration-100 ease-in-out"
                       key={index}
                       href={item.link}
                     >
